4b49178b9f8a2f150cc91e4546fddf8ce05625bd
joinnearby <joinnearby@aol.com>
2020-07-07 23:32:01 +0800

    head for pages

---
+++ js/producer.js
@@ -0,0 +1,31 @@
+var baseUrl = "/cloud/internal"
+function head() {
+	console.log('ajax head for producer')
+	jQuery.ajax({
+	    url: baseUrl + "/pages/head/producer?r=" + Math.random(),
+	    success: function( result ) {
+	      console.log("[joinnearby] producer head success " + result)
+			var data = result.data
+			var name = data.name
+			var values = data.values
+			var table = '<table class="max-table">\n' +
+				'            <tr>\n'
+			for(var i=0;i<values.length;i++) {
+				console.log("append " + values[i])
+				table = table + '                <td>' + values[i] + '</td>\n'
+			}
+			table = table + '            </tr>\n' +
+				'        </table>'
+			$("#pages-head").html(table)
+			$("#pages-head").attr("head", values.length)
+			$("#pages-head").attr("name", name)
+	    },
+	    error: function( xhr, result, obj ) {
+	      console.log("[joinnearby] stream head error " + result)
+	    }
+	})
+}
+
+
+
+head()
