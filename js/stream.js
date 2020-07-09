6524887166393a55a64789d4a94fff4807982307
joinnearby <joinnearby@aol.com>
2020-07-09 21:10:25 +0800

    data for pages

--- js/stream.js
+++ js/stream.js
@@ -1,4 +1,35 @@
 var baseUrl = "/cloud/internal"
+var title = ''
+var values = []
+var dataMap = {}
+var lastId = ''
+function data() {
+	console.log('ajax data for stream')
+	jQuery.ajax({
+		url: baseUrl + "/stream/page?pageSize=10r=" + Math.random(),
+		success: function( result ) {
+			console.log("[joinnearby] stream data success " + result)
+			var data = result.data
+			var table = '<table class="max-table">\n'
+			for(var i=0;i<data.length;i++) {
+				var obj = data[i]
+				table = table + '<tr>\n'
+				for(var j=0;j<values.length;j++) {
+					var name = values[j]
+					console.log("append obj." + name)
+					table = table + '<td>' + obj[name] + '</td>\n'
+				}
+				table = table + '</tr>\n'
+			}
+			table = table + '</table>\n'
+			$("#pages-data").html(table)
+		},
+		error: function( xhr, result, obj ) {
+			console.log("[joinnearby] stream head error " + result)
+		}
+	})
+}
+
 function head() {
 	console.log('ajax head for stream')
 	jQuery.ajax({
@@ -6,19 +37,19 @@ function head() {
 	    success: function( result ) {
 	      console.log("[joinnearby] stream head success " + result)
 			var data = result.data
-			var name = data.name
-			var values = data.values
-			var table = '<table class="max-table">\n' +
-				'            <tr>\n'
+			title = data.name
+			values = data.values
+			var table = '<table class="max-table"><tr>\n'
 			for(var i=0;i<values.length;i++) {
 				console.log("append " + values[i])
-				table = table + '                <td>' + values[i] + '</td>\n'
+				table = table + '<td>' + values[i] + '</td>\n'
 			}
-			table = table + '            </tr>\n' +
-				'        </table>'
+			table = table + '</tr></table>\n'
 			$("#pages-head").html(table)
 			$("#pages-head").attr("head", values.length)
-			$("#pages-head").attr("name", name)
+			$("#pages-head").attr("name", title)
+			$("#pages-title").text(title)
+			data()
 	    },
 	    error: function( xhr, result, obj ) {
 	      console.log("[joinnearby] stream head error " + result)
