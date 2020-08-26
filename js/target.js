443981081165dd78aaeb2acc760e1c86cb8d6bc0
joinnearby <joinnearby@aol.com>
2020-08-26 15:47:41 +0800

    logout cookie token

--- js/target.js
+++ js/target.js
@@ -58,7 +58,7 @@ function loadmore() {
 				var index = parseInt(itemid)
 				var item = dataMap[index]
 
-				var itemhtml = '<table>'
+				var itemhtml = '<table class="min-table">'
 				for(var k=0;k<values.length;k++) {
 					var iname = values[k].name
 					var ivalue = values[k].value
